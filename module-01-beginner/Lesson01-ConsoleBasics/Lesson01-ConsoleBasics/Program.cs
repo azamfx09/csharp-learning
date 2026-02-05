c7c46707690e697d444154c13c38f6d194a0c55c
azamfx09 <dd1778673@gmail.com>
2026-02-05 11:41:06 +0500

    done lesson 01

--- module-01-beginner/Lesson01-ConsoleBasics/Lesson01-ConsoleBasics/Program.cs
+++ module-01-beginner/Lesson01-ConsoleBasics/Lesson01-ConsoleBasics/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Lesson01_ConsoleBasics
+﻿using System.Xml;
+
+namespace Lesson01_ConsoleBasics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Azamat Rakhmonkulov");
+            Console.WriteLine("Hello G13");
+            Console.WriteLine("My Name Is Azamat");
+            Console.WriteLine("Learning C# .net");
+            Console.WriteLine("Join is my telegram channel");
+            Console.WriteLine("My surname is Rakhmonkulov");
+            Console.WriteLine("I am Student");
+            Console.WriteLine("mening ismim Azamat ");
+            Console.WriteLine("Men dasturchi bo'lmoqchiman");
+            Console.WriteLine("Assalomu alekum ");
+            Console.WriteLine("Green Screen");
+
+
         }
     }
 }
