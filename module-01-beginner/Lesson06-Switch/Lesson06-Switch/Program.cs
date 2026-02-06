9d2524dd3d6933d982c681f7604b26f617fa9717
azamfx09 <dd1778673@gmail.com>
2026-02-06 19:41:50 +0500

    done

--- module-01-beginner/Lesson06-Switch/Lesson06-Switch/Program.cs
+++ module-01-beginner/Lesson06-Switch/Lesson06-Switch/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choice = 2;
+
+            switch (choice)
+            {
+
+                case 1:
+                    Console.WriteLine("Start");
+                    break;
+
+
+                case 2:
+                    Console.WriteLine("Options");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Exit");
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong choice");
+                    break;
+            }
         }
     }
 }
